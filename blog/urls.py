afaa4d9b7a97a28e8ee0b2c8adfb3d4058f7e0ea
songhee-dhk <songhee.kim@deliveryhero.co.kr>
2021-07-23 15:23:08 +0900

    Added view and template for detailed blog post as wel as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.shortcuts import render
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
