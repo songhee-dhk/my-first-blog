c96d9d3229b7d9c8868687fde7962200323a53e0
songhee-dhk <songhee.kim@deliveryhero.co.kr>
2021-08-09 18:46:43 +0900

    PROL-564 create APITestMixin

--- blog/tests.py
+++ blog/tests.py
@@ -5,10 +5,21 @@ from django.utils import timezone
 from django.urls import reverse
 from http import HTTPStatus
 
+
 import json
 
 
-class TestPost(TestCase):
+class APITestMixin:
+    def get(self, path, data={}, *args, **kwargs):
+        return self.client.get(path, data, *args, **kwargs)
+
+    def post(self, path, data={}, *args, **kwargs):
+        return self.client.post(
+            path, json.dumps(data), content_type="application/json", *args, **kwargs
+        )
+
+
+class TestPost(APITestMixin, TestCase):
     def _create_post(self, user, title, text):
         post = Post.objects.create(
             author=user, title=title, text=text, published_date=timezone.now()
@@ -33,10 +44,10 @@ class TestPost(TestCase):
             self._create_post(self.user, "Post title", "Post content")
 
         # When : 모든 Post 조회
-        response = self.client.get(reverse("post_list"))
+        response = self.get(reverse("post_list"))
 
         # Then : 생성된 모든 Post가 정상적으로 조회되는지 확인
-        data = json.loads(response.json()["data"])
+        data = response.json()
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(len(data), 30)
@@ -46,7 +57,7 @@ class TestPost(TestCase):
         saved_post = self._create_post(self.user, "Post title", "Post content")
 
         # When : 생성된 Post 단건 조회
-        response = self.client.get(reverse("post_detail", kwargs={"pk": saved_post.pk}))
+        response = self.get(reverse("post_detail", kwargs={"pk": saved_post.pk}))
 
         # Then : 조회한 Post와 생성한 Post가 일치하는지 확인
         post = response.json()
@@ -61,7 +72,7 @@ class TestPost(TestCase):
         not_exist_pk = 1234
 
         # When : 존재하지 않는 Post 조회
-        response = self.client.get(reverse("post_detail", kwargs={"pk": not_exist_pk}))
+        response = self.get(reverse("post_detail", kwargs={"pk": not_exist_pk}))
 
         # Then : 404 에러를 반환
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
@@ -72,9 +83,7 @@ class TestPost(TestCase):
         data = {"title": "Post Title", "text": "Post Text"}
 
         # When : 정상적인 Post 등록
-        response = self.client.post(
-            reverse("post_new"), data=json.dumps(data), content_type="application/json"
-        )
+        response = self.post(reverse("post_new"), data)
 
         # Then : 정상적으로 Post의 값들이 생성되었는지 확인
         post = response.json()
@@ -90,11 +99,7 @@ class TestPost(TestCase):
         data = {"title": "Post Title", "text": "Post Text"}
 
         # When : Post를 생성
-        response = self.client.post(
-            reverse("post_new"),
-            data=json.dumps(data),
-            content_type="application/json",
-        )
+        response = self.post(reverse("post_new"), data)
 
         # Then : Post가 생성되지 않고 로그인 페이지로 이동하게 한다
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
@@ -105,11 +110,7 @@ class TestPost(TestCase):
         empty_reqeust_data = {}
 
         # When : 빈 Form으로 Post 생성을 요청
-        response = self.client.post(
-            reverse("post_new"),
-            data=json.dumps(empty_reqeust_data),
-            content_type="application/json",
-        )
+        response = self.post(reverse("post_new"), empty_reqeust_data)
 
         # Then : Post가 정상적으로 생성되지 않고, Bad_Request를 반환
         self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
@@ -121,10 +122,8 @@ class TestPost(TestCase):
         update_request_data = {"title": "New Title", "text": "New Text"}
 
         # When : Post 수정 내용과 함께 요청을 보냄
-        response = self.client.post(
-            reverse("post_edit", kwargs={"pk": exist_post.pk}),
-            data=json.dumps(update_request_data),
-            content_type="application/json",
+        response = self.post(
+            reverse("post_edit", kwargs={"pk": exist_post.pk}), update_request_data
         )
 
         # Then : Post의 내용이 정상적으로 수정
@@ -141,10 +140,8 @@ class TestPost(TestCase):
         update_request_data = {"title": "New Title", "text": "New Text"}
 
         # When : Post의 수정을 요청
-        response = self.client.post(
-            reverse("post_edit", kwargs={"pk": exist_post.pk}),
-            data=json.dumps(update_request_data),
-            content_type="application/json",
+        response = self.post(
+            reverse("post_edit", kwargs={"pk": exist_post.pk}), update_request_data
         )
 
         # Then : Post가 수정되지 않는다
@@ -162,10 +159,8 @@ class TestPost(TestCase):
         empty_request_data = {}
 
         # When : 빈 Form으로 Post 수정을 요청
-        response = self.client.post(
-            reverse("post_edit", kwargs={"pk": exist_post.pk}),
-            data=json.dumps(empty_request_data),
-            content_type="application/json",
+        response = self.post(
+            reverse("post_edit", kwargs={"pk": exist_post.pk}), empty_request_data
         )
 
         # Then : Bad_Request를 반환
@@ -177,9 +172,8 @@ class TestPost(TestCase):
         not_exist_pk = 1234
 
         # When : 존재하지 않는 Post의 수정을 요청
-        response = self.client.post(
+        response = self.post(
             reverse("post_edit", kwargs={"pk": not_exist_pk}),
-            content_type="application/json",
         )
 
         # Then : Not Found를 반환
@@ -193,7 +187,7 @@ class TestPost(TestCase):
         )
 
         # When : Post의 publish 요청
-        response = self.client.post(
+        response = self.post(
             reverse("post_publish", kwargs={"pk": not_published_post.pk})
         )
 
@@ -204,7 +198,7 @@ class TestPost(TestCase):
         self.assertTrue(response.json()["published_date"])
 
 
-class TestComment(TestCase):
+class TestComment(APITestMixin, TestCase):
     def _login_user(self):
         return self.client.login(username=self.username, password=self.password)
 
@@ -215,7 +209,7 @@ class TestComment(TestCase):
         self.user.set_password(self.password)
         self.user.save()
 
-        self.post = Post.objects.create(
+        self.saved_post = Post.objects.create(
             author=self.user, title="Post title", text="Post text"
         )
 
@@ -224,10 +218,9 @@ class TestComment(TestCase):
         valid_request_data = {"author": "comment author", "text": "comment text"}
 
         # When : Comment의 생성을 요청
-        response = self.client.post(
-            reverse("add_comment_to_post", kwargs={"pk": self.post.pk}),
-            data=json.dumps(valid_request_data),
-            content_type="application/json",
+        response = self.post(
+            reverse("add_comment_to_post", kwargs={"pk": self.saved_post.pk}),
+            valid_request_data,
         )
 
         # Then : 201 Created가 반환
@@ -245,24 +238,22 @@ class TestComment(TestCase):
         valid_request_data = {"author": "comment author", "text": "comment text"}
 
         # When : Comment의 생성을 요청
-        response = self.client.post(
+        response = self.post(
             reverse("add_comment_to_post", kwargs={"pk": not_exist_post_pk}),
-            data=json.dumps(valid_request_data),
-            content_type="application/json",
+            valid_request_data,
         )
 
         # Then : 404 Not found가 반환
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_return_bad_request_when_add_invalid_comment(self):
-        # Given : 정상적으로 Comment를 등록할 수 없는 데이
+        # Given : 정상적으로 Comment를 등록할 수 없는 데이터
         invalid_request_data = {}
 
         # When : 비어있는 Form으로 Comment 생성 요청
-        response = self.client.post(
-            reverse("add_comment_to_post", kwargs={"pk": self.post.pk}),
-            data=json.dumps(invalid_request_data),
-            content_type="application/json",
+        response = self.post(
+            reverse("add_comment_to_post", kwargs={"pk": self.saved_post.pk}),
+            invalid_request_data,
         )
 
         # Then : 400 Bad Request를 반환한다
